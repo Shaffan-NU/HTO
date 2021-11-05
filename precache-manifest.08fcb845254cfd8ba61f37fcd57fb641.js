16b1318e0c50a196ee8d1f91dfda29af26f05d29
shaffanFast <k190863@nu.edu.pk>
2021-11-05 15:45:26 +0500

    Updates

---
+++ precache-manifest.08fcb845254cfd8ba61f37fcd57fb641.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = [
+  {
+    "revision": "426d6e3b9b81e96eccba",
+    "url": "/HTO/static/css/main.7c54eb1b.chunk.css"
+  },
+  {
+    "revision": "426d6e3b9b81e96eccba",
+    "url": "/HTO/static/js/main.306b4a95.chunk.js"
+  },
+  {
+    "revision": "60a43e26a8d3699af44a",
+    "url": "/HTO/static/js/runtime~main.330f60c9.js"
+  },
+  {
+    "revision": "2fa9078e05432ae84f0d",
+    "url": "/HTO/static/css/2.8868260c.chunk.css"
+  },
+  {
+    "revision": "2fa9078e05432ae84f0d",
+    "url": "/HTO/static/js/2.f90e00f5.chunk.js"
+  },
+  {
+    "revision": "a73954d12550782b823ef827c6d3cc04",
+    "url": "/HTO/static/media/cartR_H.a73954d1.png"
+  },
+  {
+    "revision": "d08c52463318dd0ee1dffa8b658fef8d",
+    "url": "/HTO/static/media/cartR_T.d08c5246.png"
+  },
+  {
+    "revision": "9453a215e8569bc0d48dd800f29684ba",
+    "url": "/HTO/static/media/cartL_H.9453a215.png"
+  },
+  {
+    "revision": "3a59e185a90898933e52f25349ae4d7d",
+    "url": "/HTO/static/media/cartL_T.3a59e185.png"
+  },
+  {
+    "revision": "af18e021df0acbf0ed1934fd8437f500",
+    "url": "/HTO/static/media/background.af18e021.jpg"
+  },
+  {
+    "revision": "7360a269fa89ec8922a9fb9354820523",
+    "url": "/HTO/static/media/sound.7360a269.png"
+  },
+  {
+    "revision": "424d18986561c7039221eb97a66aef6b",
+    "url": "/HTO/static/media/tostartpressbutton.424d1898.mp3"
+  },
+  {
+    "revision": "8efff185f64e4259ae68a4074dbfb0e6",
+    "url": "/HTO/static/media/321go.8efff185.mp3"
+  },
+  {
+    "revision": "09b0b7bb4e8aad40481825ec9d91d223",
+    "url": "/HTO/static/media/rightAnswer.09b0b7bb.mp3"
+  },
+  {
+    "revision": "ddcbc59b2917e3dbeb737fca1e3ede4e",
+    "url": "/HTO/static/media/wrongAnswer.ddcbc59b.mp3"
+  },
+  {
+    "revision": "47df90f219856aa11addc7a1fdce1ea2",
+    "url": "/HTO/static/media/zero.47df90f2.png"
+  },
+  {
+    "revision": "3fda3c9da4ec590c5c88e647022a3170",
+    "url": "/HTO/static/media/one.3fda3c9d.png"
+  },
+  {
+    "revision": "5958b1ecfa6c8a21320db07c4c44221a",
+    "url": "/HTO/static/media/two.5958b1ec.png"
+  },
+  {
+    "revision": "9f699579df78dbbfbf2a4e1bff8b2ce0",
+    "url": "/HTO/static/media/three.9f699579.png"
+  },
+  {
+    "revision": "ad6cf83e9ded1abe81fb365462c8f4d7",
+    "url": "/HTO/static/media/four.ad6cf83e.png"
+  },
+  {
+    "revision": "eacd1490e0424542571b019369a01fe9",
+    "url": "/HTO/static/media/five.eacd1490.png"
+  },
+  {
+    "revision": "15ef5fbda60a9f2559b901ba1fae4485",
+    "url": "/HTO/static/media/six.15ef5fbd.png"
+  },
+  {
+    "revision": "612985c4d71cd2ef03008c8eaca76565",
+    "url": "/HTO/static/media/seven.612985c4.png"
+  },
+  {
+    "revision": "40b97ecd399aaa848af14e70689a2bed",
+    "url": "/HTO/static/media/eight.40b97ecd.png"
+  },
+  {
+    "revision": "6f04579f42ad580f201faf4ad35b0c81",
+    "url": "/HTO/static/media/nine.6f04579f.png"
+  },
+  {
+    "revision": "e0e17d693f0783d24c29a0c8383fc411",
+    "url": "/HTO/static/media/cartR_O.e0e17d69.png"
+  },
+  {
+    "revision": "722a23b1191d46966aedd78d95fca9d8",
+    "url": "/HTO/static/media/cartL_O.722a23b1.png"
+  },
+  {
+    "revision": "59843b2d65b09b144fd402b9327e37d1",
+    "url": "/HTO/static/media/instructions.59843b2d.gif"
+  },
+  {
+    "revision": "a0d6b13874a29393b0f8252f2adb5fa7",
+    "url": "/HTO/static/media/grocerySound.a0d6b138.mp3"
+  },
+  {
+    "revision": "f02ac89a44b28acd80cb6b0399adbf8f",
+    "url": "/HTO/static/media/mute.f02ac89a.png"
+  },
+  {
+    "revision": "4ca44582bf0ef7795495efe39b1bcae3",
+    "url": "/HTO/index.html"
+  }
+];
